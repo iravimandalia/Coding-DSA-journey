913bf3d93ed17563c210400bb18d596184aa32c8
iravimandalia <ravim9231@gmil.com>
2021-04-29 23:19:51 +0530

    Recursion basic theory and factorial program

---
+++ FactorialRecursion_1.java
@@ -0,0 +1,29 @@
+/*
+* Recursion in computer science is a method where the solution to a problem depends on solutions to smaller instances of the same problem.
+* 3-Step Process
+* ---------------
+* 1. Base Case
+* 2. Hypothesis
+* 3. Induction
+*
+* Step 2 & step 3 are interchangeable depending on the problem.
+*
+* With this let's get started and solve how to calculate factorial of a number using recursion.
+*/
+
+public class Factorial {
+	
+	public static int fact(int n) {
+		if(n == 0 || n == 1) //Base case
+			return 1;
+		
+		return n*fact(n-1); //Hypothesis - Induction
+	}
+
+	//Driver method
+	public static void main(String[] args) {
+		int n = 5;
+		int result = fact(n);
+		System.out.println("Factorial of " + n + " is " + result);
+	}
+}
